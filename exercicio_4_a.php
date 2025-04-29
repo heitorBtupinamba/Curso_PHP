4d2732802eaeb8de66d169d84e3d1c4568855de5
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-04-28 23:11:03 -0300

    Add files via upload

---
+++ exercicio_4_a.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Varíaveis e Contantes</title>
+</head>
+
+<body>
+<?php 
+    $nome = "Heitor";
+    $sobrenome = "Antônio";
+
+    const PAIS = "Brasil";
+
+    echo "Muito prazer, $nome $sobrenome! Você mora no ". PAIS . "." ;
+
+?>
+
+</body>
+</html>
+
+<?php 
+//Variáveis sempre começam com $
+//O segundo caractere após $ pode ser letra ou símbolo _
+//É recomendado que Variáveis sejam sempre todas escritas minúsculas e Contantes sejam sempre escritas todas maiúsculas
+?>
+ 
+
+
